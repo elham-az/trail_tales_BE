a86dad59df49f7313c3439bebc536ba1f0d02c3b
Magdaleeena <magdalena.janeckova92@gmail.com>
2024-11-21 09:28:38 +0000

    dataupdate

--- db/data/development-data/posts.js
+++ db/data/development-data/posts.js
@@ -12,7 +12,57 @@ module.exports = [
       post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
       description: "Amazing seals.",
       created_at: "2024-11-14T08:15:00Z",
-      location: "POINT(-73.945826 40.80629)",
-      location_coord: "(-73.945826, 40.80629)"
+      location: "POINT(-2.290500 53.490100)",
+      location_coord: "(-2.290500, 53.490100)"
+    },
+    {
+      username: "wildlife_watcher",
+      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
+      description: "Caught this Siberian tiger in action!",
+      created_at: "2024-11-13T14:45:00Z",
+      location: "POINT(2.274800, 53.486300)",
+      location_coord: "(2.274800, 53.486300)"
+    },
+    {
+      username: "urban_explorer",
+      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
+      description: "Caught this Siberian tiger in action!",
+      created_at: "2024-11-13T14:45:00Z",
+      location: "POINT(51.52272 -0.23923)",
+      location_coord: "(51.52272 -0.23923)"
+    },
+    {
+      username: "urban_explorer",
+      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
+      description: "Spotted this little beauty near the city park.",
+      created_at: "2024-11-12T09:20:00Z",
+      location: "POINT(51.52272 -0.23923)",
+      location_coord: "(51.52272 -0.23923)"
+    },
+    {
+      username: "birding_bob",
+      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
+      description: "Caught a Kingfisher mid-dive. Incredible precision!",
+      created_at: "2024-11-12T09:20:00Z",
+      location: "POINT(51.507514 -0.170798)",
+      location_coord: "(51.507514, -0.170798)"
+    },
+    {
+      username: "hiking_annie",
+      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
+      description: "This mountain goat posed like a champ for me.",
+      created_at: "2024-11-12T09:20:00Z",
+      location: "POINT(53.972420 -1.700180)",
+      location_coord: "(53.972420, -1.700180)"
+    },
+    {
+      username: "river_runner",
+      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
+      description: "Spotted a playful family of otters on my kayak trip.",
+      created_at: "2024-11-12T09:20:00Z",
+      location: "POINT(53,89874 -1.69236)",
+      location_coord: "(53,89874, -1.69236)"
     }
   ]
+
+
