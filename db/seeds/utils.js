98c910821a1c8871c801d0f49a1d6b8158400452
elham <elhamazizi05@gmail.com>
2024-11-22 15:49:58 +0000

    small change

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -3,21 +3,3 @@ exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
     return { created_at: new Date(created_at), ...otherProperties };
   };
   
-  exports.createRef = (arr, key, value) => {
-    return arr.reduce((ref, element) => {
-      ref[element[key]] = element[value];
-      return ref;
-    }, {});
-  };
-  
-  exports.formatPosts = (posts, idLookup) => {
-    return posts.map(({ created_by, belongs_to, ...restOfPosts }) => {
-      const post_id = idLookup[belongs_to];
-      return {
-        post_id,
-        username: created_by,
-        ...this.convertTimestampToDate(restOfPosts),
-      };
-    });
-  };
-  
