d635892431bbef4bb03105a5a9cb705e97692f02
Magdaleeena <magdalena.janeckova92@gmail.com>
2024-11-22 15:03:13 +0000

    data

--- db/data/development-data/posts.js
+++ db/data/development-data/posts.js
@@ -1,60 +1,305 @@
 module.exports = [
-    {
-      username: "nature_lover",
-      post_img: "https://media.gettyimages.com/id/993489488/photo/peregrine-falcon-adult-female-warming-its-chicks-city-church-esslingen-baden-wuerttemberg.jpg?s=612x612&w=gi&k=20&c=FXlV3zDkpidzWObT8njwXc3AfexCEa3n8_mS89a-QaY=",
-      description: "Check this amazing Peregrine falcons' nest.",
-      created_at: "2024-11-15T10:30:00Z",
-      location: "POINT(-73.94581 40.807475)",
-      location_coord: "(-73.94581, 40.807475)"
-    },
-    {
-      username: "adventure_jane",
-      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
-      description: "Amazing seals.",
-      created_at: "2024-11-14T08:15:00Z",
-      location: "POINT(-2.290500 53.490100)",
-      location_coord: "(-2.290500, 53.490100)"
-    },
-    {
-      username: "wildlife_watcher",
-      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
-      description: "Caught this Siberian tiger in action!",
-      created_at: "2024-11-13T14:45:00Z",
-      location: "POINT(2.274800 53.486300)",
-      location_coord: "(2.274800, 53.486300)"
-    },
-    {
-      username: "urban_explorer",
-      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
-      description: "Spotted this little beauty near the city park.",
-      created_at: "2024-11-12T09:20:00Z",
-      location: "POINT(51.52272 -0.23923)",
-      location_coord: "(51.52272, -0.23923)"
-    },
-    {
-      username: "birding_bob",
-      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
-      description: "Caught a Kingfisher mid-dive. Incredible precision!",
-      created_at: "2024-11-12T09:21:00Z",
-      location: "POINT(51.507514 -0.170798)",
-      location_coord: "(51.507514, -0.170798)"
-    },
-    {
-      username: "hiking_annie",
-      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
-      description: "This mountain goat posed like a champ for me.",
-      created_at: "2024-11-12T09:23:00Z",
-      location: "POINT(53.972420 -1.700180)",
-      location_coord: "(53.972420, -1.700180)"
-    },
-    {
-      username: "river_runner",
-      post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
-      description: "Spotted a playful family of otters on my kayak trip.",
-      created_at: "2024-11-12T09:22:00Z",
-      location: "POINT(53.89874 -1.69236)",
-      location_coord: "(53.89874, -1.69236)"
-    }
-  ]
+  {
+    username: "nature_lover",
+    post_img: "https://images.unsplash.com/photo-1637089860275-536802bb1bbc?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Check this amazing peregrine falcon",
+    created_at: "2024-11-15T10:30:00Z",
+    location: "POINT(54.073372 -2.158266)",
+    location_coord: "(54.073372, -2.158266)"
+  },
+  {
+    username: "adventure_jane",
+    post_img: "https://www.lincstrust.org.uk/sites/default/files/styles/scaled_default/public/2019-01/DonnaNook_Ruth%20Taylor_1.JPG?itok=AALwfbLf",
+    description: "Grey seals! A world full of adorable seal pups! You should totally visit this place.",
+    created_at: "2024-11-14T08:15:00Z",
+    location: "POINT(53.475124 0.146371)",
+    location_coord: "(53.475124, 0.146371)"
+  },
+  {
+    username: "wildlife_watcher",
+    post_img: "https://images.unsplash.com/photo-1579539772000-08269185ccec?q=80&w=2714&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Golden Eagle",
+    created_at: "2024-11-13T14:45:00Z",
+    location: "POINT(56.178969 -4.504253)",
+    location_coord: "(56.178969, -4.504253)"
+  },
+  {
+    username: "Darren_Smith",
+    post_img: "https://images.unsplash.com/photo-1570302262350-a29171477406?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Red deer",
+    created_at: "2024-11-16T09:29:00Z",
+    location: "POINT(56.993880 -3.528561)",
+    location_coord: "(56.993880, -3.528561)"
+  },
+  {
+    username: "MattO",
+    post_img: "https://images.unsplash.com/photo-1621093164959-bcc2bd695cde?q=80&w=2930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Caught a kingfisher mid-dive. Incredible precision!",
+    created_at: "2024-11-11T11:11:00Z",
+    location: "POINT(53.438023 -2.300348)",
+    location_coord: "(53.438023, -2.300348)"
+  },
+  {
+    username: "wildlife_watcher",
+    post_img: "https://images.unsplash.com/photo-1718819877341-332053bb5ab9?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Hedgehog",
+    created_at: "2024-11-06T11:27:00Z",
+    location: "POINT(53.471421 -2.277034)",
+    location_coord: "(53.471421, -2.277034)"
+  },
+  {
+    username: "Sarah678",
+    post_img: "https://www.wildlifetrusts.org/sites/default/files/styles/scaled_12_col_desk/public/2018-04/wildlifetrusts_40542445751.jpg?itok=ihMbuSYj",
+    description: "Peregrine falcon in Manchester city",
+    created_at: "2024-11-13T09:37:00Z",
+    location: "POINT(53.477786 -2.244078)",
+    location_coord: "(53.477786, -2.244078)"
+  },
+  {
+    username: "SamH",
+    post_img: "https://images.unsplash.com/photo-1518975173686-1d9d6de5afa3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Scottish wildcat",
+    created_at: "2024-11-14T08:10:00Z",
+    location: "POINT(56.873344 -3.843698)",
+    location_coord: "(56.873344, -3.843698)"
+  },
+  {
+    username: "birdEnthusiast",
+    post_img: "https://images.unsplash.com/photo-1502234633758-eb85b5c10298?q=80&w=2736&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Osprey",
+    created_at: "2024-11-03T14:20:00Z",
+    location: "POINT(55.188173 -2.489712)",
+    location_coord: "(55.188173, -2.489712)"
+  },
+  {
+    username: "Colin_Green",
+    post_img: "https://images.unsplash.com/photo-1530558497469-32ceb7ddaae3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Puffins by Cymru Ynys Lawd",
+    created_at: "2024-11-12T10:37:00Z",
+    location: "POINT(53.305540 -4.693307)",
+    location_coord: "(53.305540, -4.693307)"
+  },
+  {
+    username: "Ian_Birder",
+    post_img: "https://images.unsplash.com/photo-1561218679-4e9f00483e0c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Long-eared owl",
+    created_at: "2024-11-12T07:54:00Z",
+    location: "POINT(53.333478 -1.817602)",
+    location_coord: "(53.333478, -1.817602)"
+  },
+  {
+    username: "nature_lover",
+    post_img: "https://images.unsplash.com/photo-1688224677170-1e6058ac0fcb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fGZhbGxvdyUyMGRlZXJ8ZW58MHx8MHx8fDA%3D",
+    description: "fallow deer",
+    created_at: "2024-11-14T15:55:00Z",
+    location: "POINT(51.416591 -0.341484)",
+    location_coord: "(51.416591, -0.341484)"
+  },
+  {
+    username: "MattO",
+    post_img: "https://images.unsplash.com/photo-1464038293999-0fb8953380d9?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "cute bunny",
+    created_at: "2024-11-19T11:55:00Z",
+    location: "POINT(51.541446 -0.240591)",
+    location_coord: "(51.541446, -0.240591)"
+  },
+  {
+    username: "wildlife_watcher",
+    post_img: "https://images.unsplash.com/photo-1572896954319-785d85e13cb9?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "fox roaming the streets of London",
+    created_at: "2024-11-19T11:55:00Z",
+    location: "POINT(51.541273 -0.251825)",
+    location_coord: "(51.541273, -0.251825)"
+  },
+  {
+    username: "Olly92",
+    post_img: "https://images.unsplash.com/photo-1691240539536-37cdf76f3738?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Egyptian geese",
+    created_at: "2024-11-20T12:14:00Z",
+    location: "POINT(51.568501 -0.250800)",
+    location_coord: "(51.568501, -0.250800)"
+  },
+  {
+    username: "Judie",
+    post_img: "https://images.unsplash.com/photo-1709157343825-4286440b2245?q=80&w=2795&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "siskin",
+    created_at: "2024-11-10T16:14:00Z",
+    location: "POINT(51.521883 -0.243007)",
+    location_coord: "(51.521883, -0.243007)"
+  },
+  {
+    username: "Mark96",
+    post_img: "https://images.unsplash.com/photo-1702600025768-b5fc745a22ed?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "greenfinch ",
+    created_at: "2024-11-18T12:14:00Z",
+    location: "POINT(51.546303 -0.324279)",
+    location_coord: "(51.546303, -0.324279)"
+  },
+  {
+    username: "BethT",
+    post_img: "https://upload.wikimedia.org/wikipedia/commons/9/93/Grampo.jpg",
+    description: "Risso's dolphin",
+    created_at: "2024-11-06T12:19:00Z",
+    location: "POINT(52.801117 -4.695627)",
+    location_coord: "(52.801117, -4.695627)"
+  },
+  {
+    username: "Ian_Birder",
+    post_img: "https://images.unsplash.com/photo-1661460679632-2dc2db517e47?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Tawny owl",
+    created_at: "2024-11-02T11:19:00Z",
+    location: "POINT(53.154456 -2.419991)",
+    location_coord: "(53.154456, -2.419991)"
+  },
+  {
+    username: "KeithC",
+    post_img: "https://images.unsplash.com/photo-1697392301298-53dc911f436c?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "My first post here - Egrets",
+    created_at: "2024-11-01T11:19:00Z",
+    location: "POINT(53.181764 -2.305404)",
+    location_coord: "(53.181764, -2.305404)"
+  },
+  {
+    username: "river_runner",
+    post_img: "https://images.unsplash.com/photo-1698435354379-b10bcc803369?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "otters",
+    created_at: "2024-11-01T11:19:00Z",
+    location: "POINT(53.131512 -2.411105)",
+    location_coord: "(53.131512, -2.411105)"
+  },
+  {
+    username: "nature_lover",
+    post_img: "https://images.unsplash.com/photo-1560510052-1cbb08298fd6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fG1hcnRlbnN8ZW58MHx8MHx8fDA%3D",
+    description: "European Pine Marten",
+    created_at: "2024-11-09T13:11:00Z",
+    location: "POINT(53.983508 -2.618967)",
+    location_coord: "(53.983508, -2.618967)"
+  },
+  {
+    username: "nature_lover",
+    post_img: "https://images.unsplash.com/photo-1673009179657-63d4fd49a2f4?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Spoonbill having lunch",
+    created_at: "2024-11-09T13:11:00Z",
+    location: "POINT(53.983508 -2.618967)",
+    location_coord: "(53.983508, -2.618967)"
+  },
+  {
+    username: "hiking_annie",
+    post_img: "https://images.unsplash.com/photo-1521673107607-1ca9a8184520?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Robin in the garden",
+    created_at: "2024-11-15T11:11:00Z",
+    location: "POINT(53.899092 -1.620386)",
+    location_coord: "(53.899092, -1.620386)"
+  },
+  {
+    username: "adventure_jane",
+    post_img: "https://images.unsplash.com/photo-1600892457290-84afbce878f0?q=80&w=2783&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "barn owl",
+    created_at: "2024-11-16T11:11:00Z",
+    location: "POINT(53.925208 -1.662278)",
+    location_coord: "(53.925208, -1.662278)"
+  },
+  {
+    username: "Sarah678",
+    post_img: "https://images.unsplash.com/photo-1516715069507-bfb28b3138ff?q=80&w=2502&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "deer near Harewood house",
+    created_at: "2024-11-19T15:51:00Z",
+    location: "POINT(53.896675 -1.519746)",
+    location_coord: "(53.896675, -1.519746)"
+  },
+  {
+    username: "birdEnthusiast",
+    post_img: "https://images.unsplash.com/photo-1680439582202-1db7fae8e372?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "little owl",
+    created_at: "2024-11-19T15:51:00Z",
+    location: "POINT(53.895320 -1.669430)",
+    location_coord: "(53.895320, -1.669430)"
+  },
+  {
+    username: "nature_lover",
+    post_img: "https://images.unsplash.com/photo-1557090641-0584606a05c2?q=80&w=2624&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "busy beaver",
+    created_at: "2024-11-19T15:51:00Z",
+    location: "POINT(50.793694 -3.228739)",
+    location_coord: "(50.793694, -3.228739)"
+  },
+  {
+    username: "MattO",
+    post_img: "https://images.unsplash.com/photo-1580028770404-8e91010a39ca?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "water vole is so adorable",
+    created_at: "2024-11-19T10:51:00Z",
+    location: "POINT(50.865967 -0.545796)",
+    location_coord: "(50.865967, -0.545796)"
+  },
+  {
+    username: "SamH",
+    post_img: "https://images.unsplash.com/photo-1536235323068-373c29a9e9f8?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Smart squirrel",
+    created_at: "2024-11-17T10:10:00Z",
+    location: "POINT(50.854580 -1.594389)",
+    location_coord: "(50.854580, -1.594389)"
+  },
+  {
+    username: "birdEnthusiast",
+    post_img: "https://images.unsplash.com/photo-1647974096876-52ff033cac23?q=80&w=2912&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Oystercatcher near Grassington",
+    created_at: "2024-11-17T11:10:00Z",
+    location: "POINT(54.065180 -1.989095)",
+    location_coord: "(54.065180, -1.989095)"
+  },
+  {
+    username: "river_runner",
+    post_img: "https://images.unsplash.com/photo-1637054766297-34894d776300?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Kyte near Swinsty reservoir",
+    created_at: "2024-11-16T09:44:00Z",
+    location: "POINT(53.975713 -1.714774)",
+    location_coord: "(53.975713, -1.714774)"
+  },
+  {
+    username: "hiking_annie",
+    post_img: "https://images.unsplash.com/photo-1541726019318-a15693fe20a2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "I have seen a hawk on my hike to the top of Pen-y-ghent",
+    created_at: "2024-11-16T09:44:00Z",
+    location: "POINT(54.155616 -2.218938)",
+    location_coord: "(54.155616, -2.218938)"
+  },
+  {
+    username: "MattO",
+    post_img: "https://images.unsplash.com/photo-1516437061788-ef67ae4b0dc4?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Great blue heron",
+    created_at: "2024-11-18T09:44:00Z",
+    location: "POINT(53.382619 -1.869546)",
+    location_coord: "(53.382619, -1.869546)"
+  },
+  {
+    username: "hiking_annie",
+    post_img: "https://images.unsplash.com/photo-1634330843575-1ecfbd0f488c?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "curlew",
+    created_at: "2024-11-18T09:44:00Z",
+    location: "POINT(52.766587 -3.722232)",
+    location_coord: "(52.766587, -3.722232)"
+  },
+  {
+    username: "wildlife_watcher",
+    post_img: "https://images.unsplash.com/photo-1634506265052-fde82055ce40?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "avocet",
+    created_at: "2024-11-14T11:37:00Z",
+    location: "POINT(51.074981 0.383226)",
+    location_coord: "(51.074981, 0.383226)"
+  },
+  {
+    username: "nature_lover",
+    post_img: "https://images.unsplash.com/photo-1654445112615-fe49f9d1368a?q=80&w=2924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "razorbill to be seen on Bempton Cliffs",
+    created_at: "2024-11-14T11:37:00Z",
+    location: "POINT(54.149051 -0.165512)",
+    location_coord: "(54.149051, -0.165512)"
+  }
+]
+
+
+
+
+
 
 
--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,48 +2,103 @@ module.exports =[
     {
     username: 'nature_lover',
     name: 'Alina',
-    profile_img: 'https://plus.unsplash.com/premium_photo-1668110864450-48a6591c3a22?q=80&w=2804&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    points: 30},
+    profile_img: 'https://images.unsplash.com/photo-1622165339419-ac4a595e90e2?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    points: 30
+    },
     {
     username: 'adventure_jane',
     name: 'Jane',
-    profile_img: 'https://images.pexels.com/photos/27198641/pexels-photo-27198641/free-photo-of-a-beach-with-boats-and-a-cliff-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
+    profile_img: 'https://images.pexels.com/photos/29430746/pexels-photo-29430746/free-photo-of-woman-exploring-scenic-mountainscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     points: 12
     },
     {
     username: 'birdEnthusiast',
     name: 'Thomas',
-    profile_img: 'https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
+    profile_img: 'https://images.unsplash.com/photo-1515041219749-89347f83291a?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     points: 30
     }, 
     {
     username: 'wildlife_watcher',
-    name: 'Lana',
-    profile_img: 'https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
-    points: 42
+    name: 'Maryam',
+    profile_img: 'https://images.pexels.com/photos/20700696/pexels-photo-20700696/free-photo-of-sun-shining-over-a-lake.jpeg?auto=compress&cs=tinysrgb&w=1200',
+    points: 16
     }, 
     {
-    username: 'urban_explorer',
-    name: 'Thomas',
-    profile_img: 'https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
+    username: 'Sarah678',
+    name: 'Sarah',
+    profile_img: 'https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg?auto=compress&cs=tinysrgb&w=1200',
     points: 15
     }, 
     {
-    username: 'birding_bob',
-    name: 'Bob',
-    profile_img: 'https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
+    username: 'SamH',
+    name: 'Samuel Howard',
+    profile_img: 'https://images.pexels.com/photos/236599/pexels-photo-236599.jpeg?auto=compress&cs=tinysrgb&w=1200',
     points: 12
     },
     {
     username: 'hiking_annie',
     name: 'Annie',
     profile_img: 'https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
-    points: 15
+    points: 35
     },
     {
     username: 'river_runner',
     name: 'Jess',
-    profile_img: 'https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
+    profile_img: 'https://images.pexels.com/photos/556669/pexels-photo-556669.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     points: 8
-    }
+    },
+    {
+    username: 'Judie',
+    name: 'Judie Smith',
+    profile_img: 'https://images.unsplash.com/photo-1642018176342-f929f4327c8d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    points: 3
+    },
+    {
+    username: 'Colin_Green',
+    name: 'Colin Green',
+    profile_img: 'https://images.unsplash.com/photo-1717732596477-04f8c5d53387?q=80&w=2777&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    points: 3
+    },
+    {
+    username: 'MattO',
+    name: 'Matthew Owen',
+    profile_img: 'https://images.unsplash.com/photo-1532202802379-df93d543bac3?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    points: 26
+    },
+    {
+    username: 'Darren_Smith',
+    name: 'Darren Smith',
+    profile_img: 'https://images.unsplash.com/photo-1524481905007-ea072534b820?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    points: 8
+    },
+    {
+    username: 'Olly92',
+    name: 'Olly Hutchinson',
+    profile_img: 'https://images.pexels.com/photos/172289/pexels-photo-172289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    points: 3
+    },
+    {
+    username: 'BethT',
+    name: 'Beth Stephens',
+    profile_img: 'https://images.pexels.com/photos/235990/pexels-photo-235990.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    points: 9
+    },
+    {
+    username: 'Ian_Birder',
+    name: 'Ian Ellwood',
+    profile_img: 'https://images.pexels.com/photos/681467/pexels-photo-681467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    points: 3
+    },
+    {
+    username: 'Mark96',
+    name: 'Mark Taylor',
+    profile_img: 'https://images.unsplash.com/photo-1718567074329-ae4d92befb66?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    points: 9
+    },
+    {
+    username: 'KeithC',
+    name: 'Keith Cole',
+    profile_img: 'https://images.unsplash.com/photo-1437964706703-40b90bdf563b?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    points: 3
+    }   
 ]
