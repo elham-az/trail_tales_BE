6817db51b8ae49fdfc229e6b7b9fca900a9bd353
Magdaleeena <magdalena.janeckova92@gmail.com>
2024-11-26 20:25:59 +0000

    points

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -3,43 +3,43 @@ module.exports =[
     username: 'nature_lover',
     name: 'Alina',
     profile_img: 'https://images.unsplash.com/photo-1622165339419-ac4a595e90e2?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    points: 30
+    points: 18
     },
     {
     username: 'adventure_jane',
     name: 'Jane',
     profile_img: 'https://images.pexels.com/photos/29430746/pexels-photo-29430746/free-photo-of-woman-exploring-scenic-mountainscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    points: 12
+    points: 6
     },
     {
     username: 'birdEnthusiast',
     name: 'Thomas',
     profile_img: 'https://images.unsplash.com/photo-1515041219749-89347f83291a?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    points: 30
+    points: 12
     }, 
     {
     username: 'wildlife_watcher',
     name: 'Maryam',
     profile_img: 'https://images.pexels.com/photos/20700696/pexels-photo-20700696/free-photo-of-sun-shining-over-a-lake.jpeg?auto=compress&cs=tinysrgb&w=1200',
-    points: 18
+    points: 12
     }, 
     {
     username: 'Sarah678',
     name: 'Sarah',
     profile_img: 'https://images.pexels.com/photos/757889/pexels-photo-757889.jpeg?auto=compress&cs=tinysrgb&w=1200',
-    points: 15
+    points: 6
     }, 
     {
     username: 'SamH',
     name: 'Samuel Howard',
     profile_img: 'https://images.pexels.com/photos/236599/pexels-photo-236599.jpeg?auto=compress&cs=tinysrgb&w=1200',
-    points: 12
+    points: 6
     },
     {
     username: 'hiking_annie',
     name: 'Annie',
     profile_img: 'https://images.pexels.com/photos/27054224/pexels-photo-27054224/free-photo-of-motorbike-parked-on-hill-on-sea-coast.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
-    points: 33
+    points: 9
     },
     {
     username: 'river_runner',
@@ -63,31 +63,31 @@ module.exports =[
     username: 'MattO',
     name: 'Matthew Owen',
     profile_img: 'https://images.unsplash.com/photo-1532202802379-df93d543bac3?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    points: 24
+    points: 12
     },
     {
     username: 'Darren_Smith',
     name: 'Darren Smith',
     profile_img: 'https://images.unsplash.com/photo-1524481905007-ea072534b820?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    points: 9
+    points: 3
     },
     {
     username: 'Olly92',
     name: 'Olly Hutchinson',
     profile_img: 'https://images.pexels.com/photos/172289/pexels-photo-172289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    points: 15
+    points: 3
     },
     {
     username: 'BethT',
     name: 'Beth Stephens',
     profile_img: 'https://images.pexels.com/photos/235990/pexels-photo-235990.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    points: 9
+    points: 3
     },
     {
     username: 'Ian_Birder',
     name: 'Ian Ellwood',
     profile_img: 'https://images.pexels.com/photos/681467/pexels-photo-681467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    points: 3
+    points: 6
     },
     {
     username: 'Mark96',
@@ -102,3 +102,4 @@ module.exports =[
     points: 3
     }   
 ]
+  
